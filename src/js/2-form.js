037dceb3ce9227943b0e8b1457c0d5fff6b9fd96
Oleksii Kyryk <kirikoleksiy112@gmail.com>
2025-07-11 18:16:41 +0300

    complete 9 hw

---
+++ src/js/2-form.js
@@ -0,0 +1,45 @@
+import validator from 'validator';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+
+const email = form.elements.email;
+const message = form.elements.message;
+
+const savedData = JSON.parse(localStorage.getItem(localStorageKey));
+if (savedData) {
+  email.value = savedData.email || '';
+  message.value = savedData.message || '';
+  formData.email = savedData.email || '';
+  formData.message = savedData.message || '';
+}
+~form.addEventListener('input', evt => {
+  if (evt.target.nodeName === 'INPUT') {
+    formData.email = evt.target.value;
+  } else if (evt.target.nodeName === 'TEXTAREA') {
+    formData.message = evt.target.value;
+  }
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  if (!validator.isEmail(email.value)) {
+    alert('Invalid email!');
+  }
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  } else {
+    console.log(formData);
+    localStorage.removeItem(localStorageKey);
+  }
+  form.reset();
+  formData.email = '';
+  formData.message = '';
+});
